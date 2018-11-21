44625eca8334a7f79fb60f395187f7605a91cb1e
Doarakko <2wua4nlyi@gmail.com>
2018-11-21 16:16:43 +0900

    init

---
+++ gcalender_utility.py
@@ -0,0 +1,5 @@
+# -*- coding: utf-8 -*-
+
+
+def hoge():
+    print("hoge")
---
+++ prac01.py
@@ -0,0 +1,54 @@
+# -*- coding: utf-8 -*-
+from kaggle.api.kaggle_api_extended import KaggleApi
+
+import gcalender_utility
+
+
+# 以下週一で実行
+# 1. コンペのリストをとってくる
+# 2. すでにカレンダーに追加されているかチェック
+# 3. あったら追加
+#   - 予定名：コンペ名（competition_name）
+#   - 説明欄: コンペのURL（competition_url）
+# logger
+# google calender作成utility
+# google calender取得utility
+
+
+def get_competition_list():
+    api = KaggleApi()
+    api.authenticate()
+
+    result = api.competitions_list()
+
+    return result
+
+
+def create_event(competition_list):
+    """
+    hige
+
+    Parameters
+    ----------
+    competition_list : object
+    """
+
+    for competition_info in competition_list:
+        category = getattr(competition_info, 'category')
+        competition_name = getattr(competition_info, 'ref')
+
+        if category == 'Featured' and not exists_gcalender(competition_name):
+            competition_url = 'https://www.kaggle.com/c/' + competition_name
+            deadline = getattr(competition_info, 'deadline')
+
+
+def exists_gcalender(competition_name):
+    print('hoge')
+
+
+def main():
+    competition_list = get_competition_list()
+    create_event(competition_list)
+
+
+main()
