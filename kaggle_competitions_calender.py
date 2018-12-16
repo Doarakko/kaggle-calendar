813606fb86c8163fe0e08b0b539e76c680520682
Doarakko <2wua4nlyi@gmail.com>
2018-12-16 18:34:30 +0900

    create_events

--- kaggle_competitions_calender.py
+++ kaggle_competitions_calender.py
@@ -4,12 +4,20 @@ import json
 from kaggle.api.kaggle_api_extended import KaggleApi
 
 import datetime
+from pytz import timezone
 from googleapiclient.discovery import build
 from httplib2 import Http
 from oauth2client import file, client, tools
 
-CALENDER_SCOPES = 'https://www.googleapis.com/auth/calendar.readonly'
+CALENDER_SCOPES = 'https://www.googleapis.com/auth/calendar'
 CALENDER_ID = 'rvilq00v5vsgdvpt5arso3d4m4@group.calendar.google.com'
+
+store = file.Storage('token.json')
+creds = store.get()
+if not creds or creds.invalid:
+    flow = client.flow_from_clientsecrets('./credentials.json', CALENDER_SCOPES)
+    creds = tools.run_flow(flow, store)
+service = build('calendar', 'v3', http=creds.authorize(Http()))
 
 
 def get_competitions_list(category='featured'):
@@ -19,14 +27,6 @@ def get_competitions_list(category='featured'):
 
 
 def get_events_name():
-    store = file.Storage('token.json')
-    creds = store.get()
-    if not creds or creds.invalid:
-        flow = client.flow_from_clientsecrets(
-            './credentials.json', CALENDER_SCOPES)
-        creds = tools.run_flow(flow, store)
-    service = build('calendar', 'v3', http=creds.authorize(Http()))
-
     now = datetime.datetime.utcnow().isoformat() + 'Z'
     events_result = service.events().list(calendarId=CALENDER_ID, timeMin=now,
                                           maxResults=10, singleEvents=True,
@@ -50,11 +50,34 @@ def create_events(competitions_list):
         if competition_name not in events_name:
             # 大会 URL
             competition_url = 'https://www.kaggle.com/c/' + competition_name
+
             # 開始日時
-            enabledDate = getattr(competition_info, 'enabledDate')
+            start_date = getattr(competition_info, 'enabledDate')
+            start_date = timezone('UTC').localize(start_date)
+            start_date = start_date.isoformat()
+
             # 終了日時
-            deadline = getattr(competition_info, 'deadline')
-            print(competition_name)
+            end_date = getattr(competition_info, 'deadline')
+            end_date = timezone('UTC').localize(end_date)
+            end_date = end_date.isoformat()
+
+            body = {
+                'summary': competition_name,
+                'description': competition_url,
+                'start': {
+                    'dateTime': start_date,
+                    'timeZone': 'America/Los_Angeles',
+                },
+                'end': {
+                    'dateTime': end_date,
+                    'timeZone': 'America/Los_Angeles',
+                },
+                'reminders': {
+                    'useDefault': False,
+                },
+                'visibility': 'public',
+            }
+            event = service.events().insert(calendarId=CALENDER_ID, body=body).execute()
 
 
 def main():
